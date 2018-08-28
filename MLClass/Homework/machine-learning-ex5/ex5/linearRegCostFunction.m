dd8fa30380c526bb1b451ca71cf1b9bd8169f0a8
Lorwy <598308224@qq.com>
2018-08-28 09:21:19 +0800

    1、学习曲线 2、lambda、多项式优化等

--- MLClass/Homework/machine-learning-ex5/ex5/linearRegCostFunction.m
+++ MLClass/Homework/machine-learning-ex5/ex5/linearRegCostFunction.m
@@ -19,12 +19,30 @@ grad = zeros(size(theta));
 %               You should set J to the cost and grad to the gradient.
 %
 
+theta1 = [0; theta(2:end)];
 
+Theta = theta;
+for i =2:m
+	Theta = [Theta,theta];
+end
 
-J = 1 / (2 * m) * sum((X * theta - y) .^ 2) + lambda / (2 * m) * sum(theta .^ 2);
+h = sum(X .* Theta', 2);
+ 
+J = sum((h - y) .^ 2, 1) / m / 2 + sum(theta1 .^ 2) * lambda / 2 / m;
 
+%J = 1 / (2 * m) * sum((X * theta - y) .^ 2) + lambda / (2 * m) * sum(theta .^ 2);
 
+[m1, n1] = size(X);
 
+tmp = h - y;
+for i = 2:n1
+	tmp = [tmp, h - y];
+end
+
+grad_tmp = tmp .* X;
+grad = sum(grad_tmp, 1) / m + (lambda * theta1 / m)';
+
+grad = grad';
 
 
 
